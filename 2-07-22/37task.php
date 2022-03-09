50da1429742bbf6f1610e4d306eef0a0fa11f06c
somay-addweb <somay.s.addweb@gmail.com>
2022-03-09 10:50:21 +0530

    done

---
+++ 2-07-22/37task.php
@@ -0,0 +1,8 @@
+<?php    
+$t=$_POST['n'];   
+for($i=1; $i<=10; $i++)   
+{   
+  echo $i*$t;   
+  echo '<br>';     
+}  
+?>
--- 2-07-22/39task.php
+++ 2-07-22/39task.php
@@ -1,8 +1,9 @@
-<?php    
-$t=$_POST['n'];   
-for($i=1; $i<=10; $i++)   
-{   
-  echo $i*$t;   
-  echo '<br>';     
-}  
+<?php
+$n=33;
+$c=2;
+for ($x = 0; $c <= $n; $x++) {
+    
+  echo "$c ";
+  $c = $c+2;
+}
 ?>
